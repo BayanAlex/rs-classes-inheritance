2ea6eb26a9dd656cb566a2ad0d6fa4408206b234
cyberalex <bayanalex@gmail.com>
2022-12-08 18:35:30 +0200

    feat: added index file, connected script.js feat: made intbuilder tests fix: corrected intbuilder divide

--- classes-inheritance/script.js
+++ classes-inheritance/script.js
@@ -1,3 +1,5 @@
+// *** BASIC CLASS Builder ***
+
 function Builder(value) {
     this.value = value;
 }
@@ -15,9 +17,11 @@ Builder.prototype.get = function() {
 
 // sets new value
 Builder.prototype.set = function(value) {
-    this.value = value;
+    this.value = value || 0;
     return this;
 }
+
+// *** DERIVED ES6 CLASS IntBuilder ***
 
 class IntBuilder extends Builder {
     constructor(value) {
@@ -38,7 +42,7 @@ class IntBuilder extends Builder {
 
     // leaves integer part of division stored value on n
     divide(n) {
-        this.value /= n;
+        this.value = Math.trunc(this.value / n);
         return this;
     }
 
@@ -55,6 +59,8 @@ class IntBuilder extends Builder {
         return Math.floor(Math.random() * (to - from + 1) + from);
     }
 }
+
+// *** DERIVED ES5 CLASS StringBuilder ***
 
 function StringBuilder(value) {
     Builder.call(this, value || '');
@@ -94,3 +100,44 @@ StringBuilder.prototype.sub = function(from, n) {
     this.value = this.value.slice(from, from + n);
     return this;
 }
+
+// *** TESTS ***
+
+// IntBuilder tests
+
+let intBuilder = new IntBuilder(10);    // 10;
+intBuilder
+    .plus(2, 3, 2)                      // 17;
+    .minus(1, 2)                        // 14;
+    .multiply(2)                        // 28;
+    .divide(4)                          // 7;
+    .mod(3)                             // 1;
+console.log(intBuilder.get());          // -> 1
+
+intBuilder
+    .set(13)                            // 13
+    .mod(5)                             // 3
+    .multiply(-4)                       // -12
+    .minus(8)                           // -20
+    .plus(1, 6)                         // -13
+    .divide(3)                          // -4
+    .divide(-3)                         // 1
+console.log(intBuilder.get());          // -> 1
+
+intBuilder = new IntBuilder();          // 0
+intBuilder
+    .plus(5, 5)                         // 10
+    .divide(0)                          // Infinity
+console.log(intBuilder.get());          // -> Infinity
+
+intBuilder = new IntBuilder(5);         // 5
+intBuilder
+    .plus()                             // 5
+    .multiply()                         // NaN
+console.log(intBuilder.get());          // -> NaN
+
+for(let i = 0; i < 10; i++) {
+    console.log(IntBuilder.random(4, 10));
+}
+
+// StringBuilder tests
